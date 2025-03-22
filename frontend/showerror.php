9d2010086c74521ea34ac4605f4506201c148cbb
Shreyansh Kumar <shreykum@adobe.com>
2025-03-22 14:46:47 +0530

    Update showerror.php

--- frontend/showerror.php
+++ frontend/showerror.php
@@ -1,5 +1,6 @@
 <?php
 // Turn on error reporting for all errors, including warnings
-error_reporting(E_ALL); // Report all types of errors, including warnings
+//E_ALL & ~E_DEPRECATED & ~E_NOTICE 
+error_reporting(E_ALL & ~E_DEPRECATED); // Report all types of errors, including warnings
 ini_set('display_errors', 1); // Display errors on the screen
 ?>
