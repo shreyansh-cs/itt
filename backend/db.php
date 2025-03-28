9437aa2e9fd8f477c5a0a9d63074bb3b0a2099e3
Shreyansh Kumar <shreykum@adobe.com>
2025-03-28 18:51:33 +0530

    Change to localhost

--- backend/db.php
+++ backend/db.php
@@ -1,9 +1,10 @@
 <?php
 // db.php
-$servername = "193.203.184.227";
+$servername = "localhost";
 $username = "u760896062_root";       // अपने DB username के अनुसार सेट करें
 $db_password = "Normaxin@321";           // अपने DB password के अनुसार सेट करें
 $dbname = "u760896062_itt_education";
+//$dbname = "test";
 
 // Create connection
 $conn = new mysqli($servername, $username, $db_password, $dbname);
