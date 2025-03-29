7a07bf17780c0c10885f9b193ce714f35b384c3f
Shreyansh Kumar <shreykum@adobe.com>
2025-03-29 15:53:26 +0530

    Update db.php

--- backend/db.php
+++ backend/db.php
@@ -1,5 +1,5 @@
 <?php
-include '../../db_cred.php';
+include '../../db_cred.php'; // Use include instead of include_once, otherwise variables will not be available in some scropes
 $conn = new mysqli($servername, $username, $db_password, $dbname);
 
 // Check connection
