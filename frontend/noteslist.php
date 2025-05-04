cdfb18b444f5feab21d8e7b7406dbf04d38e7d6c
Shreyansh Kumar <shreykum@adobe.com>
2025-05-04 16:02:53 +0530

    adapt to bootstrap css

--- frontend/noteslist.php
+++ frontend/noteslist.php
@@ -9,7 +9,7 @@ $title = "Notes & Video";
     include_once '../backend/utils.php';
     $border = "1";
     include_once 'selection.php';
-    echo "<div class='container mt-4'>";
+    echo "<div class='container text-start mt-4'>";
     echo "<div class='table-responsive'>";
 
     if(!empty($chapter))
@@ -17,11 +17,11 @@ $title = "Notes & Video";
         $rows = getNotesForChapter($class,$stream,$subject,$chapter);
         echo "<table class='table table-bordered table-hover'>";
         echo "<thead class='table-primary'>";
-        echo "<tr><th>Title</th><th>Notes Download</th>";
+        echo "<tr><th style='width: 50%'>Title</th><th style='width: 25%'>Notes Download</th>";
         //Action column is only for admin
         if(isAdminLoggedIn())
         {
-            echo "<th>Action</th>";//table and headers start
+            echo "<th style='width: 25%'>Action</th>";//table and headers start
         }
         echo "</tr></thead><tbody>";
         //Each row
@@ -32,26 +32,21 @@ $title = "Notes & Video";
             echo "<span>".$row['NAME']."</span>";
             echo "</td>";
             
-            // echo "<td>";
-            // echo "<p>".$row['TEXT']."</p>";
-            // echo "</td>";
-
             echo "<td>";
-
             if(isAdminLoggedIn() || isTeacherLoggedIn() || doesUserHasSubscription($error))
             { 
-                echo "<a class='btn btn-primary rounded-pill px-4' target='_blank' href='download.php?noteid=".$row['ID']."'>DOWNLOAD PDF</a>";   
+                echo "<a class='btn btn-primary btn-sm rounded-pill px-3' target='_blank' href='download.php?noteid=".$row['ID']."'>DOWNLOAD PDF</a>";   
             }
             else
             {
-                echo "<a class='btn btn-primary rounded-pill px-4' target='_blank' href='receipts.php'>Buy Package</a>";
+                echo "<a class='btn btn-primary btn-sm rounded-pill px-3' target='_blank' href='receipts.php'>Buy Package</a>";
             }
             echo "</td>";
 
             if(isAdminLoggedIn())
             {
                 echo "<td>";
-                echo "<a class='btn btn-danger rounded-pill px-4' href='delete_note.php?noteid=".$row['ID']."'>Delete</a>";
+                echo "<a class='btn btn-danger btn-sm rounded-pill px-3' href='delete_note.php?noteid=".$row['ID']."'>Delete</a>";
                 echo "</td>";
             }
 
@@ -62,33 +57,34 @@ $title = "Notes & Video";
         echo "<br/>";
 
         //Start video section
-        echo "<table  border='0'>";
-        echo "<tr>";
-        echo "<td id='videocolumn'>";
+        echo "<div class='table-responsive'>";
         $rows = getVideoForChapter($class,$stream,$subject,$chapter);
-        echo "<table  border='$border' class='video_data'><th>Video Title</th><th>Link</th>";
+        echo "<table class='table table-bordered table-hover'>";
+        echo "<thead class='table-primary'>";
+        echo "<tr><th style='width: 50%'>Video Title</th><th style='width: 25%'>Link</th>";
         
         //action only for admin
         if(isAdminLoggedIn())
         {
-            echo "<th>Action</th>";
+            echo "<th style='width: 25%'>Action</th>";
         }
+        echo "</tr></thead><tbody>";
         foreach ($rows as $row) {
             echo "<tr>";
 
             echo "<td>";
-            echo "<h3>".$row['NAME']."</h3>";
+            echo "<span>".$row['NAME']."</span>";
             echo "</td>";
 
             echo "<td>";
             if(isAdminLoggedIn() || isTeacherLoggedIn() || doesUserHasSubscription($error))
             { 
                 $link = $row['LINK'];
-                echo "<a class='notes_link' target='_blank' href='$link'>Video</a>"; 
+                echo "<a class='btn btn-primary btn-sm rounded-pill px-3' target='_blank' href='$link'>Video</a>"; 
             }
             else
             {
-                echo "<a class='notes_link' target='_blank' href='receipts.php'>Buy Package</a>";
+                echo "<a class='btn btn-primary btn-sm rounded-pill px-3' target='_blank' href='receipts.php'>Buy Package</a>";
             }
             echo "</td>";
 
@@ -96,15 +92,13 @@ $title = "Notes & Video";
             if(isAdminLoggedIn())
             {
                 echo "<td>";
-                echo "<a class='notes_link' target='_blank' href='delete.php?class=$class&stream=$stream&subject=$subject&section=$section&chapter=$chapter&videoid=".$row['ID']."'>Delete</a>";
+                echo "<a class='btn btn-danger btn-sm rounded-pill px-3' href='delete.php?class=$class&stream=$stream&subject=$subject&section=$section&chapter=$chapter&videoid=".$row['ID']."'>Delete</a>";
                 echo "</td>";
             }
             echo "</tr>";
         }
-        echo "</table>";
-        echo "</td>";
-        echo "</tr>";
-        echo "</table>";
+        echo "</tbody></table>";
+        echo "</div>";
         //End Video section
     }
     echo "</div></div>";
@@ -112,8 +106,8 @@ $title = "Notes & Video";
     {
         echo "<div class='container text-start mt-4'>";
         echo "<div class='d-flex flex-wrap gap-3'>";
-        echo "<a class='btn btn-primary rounded-pill px-4' target='_blank' href='uploadnotes.php?class=$class&stream=$stream&subject=$subject&section=$section&chapter=$chapter'>Add New Notes</a>";
-        echo "<a class='btn btn-primary rounded-pill px-4' target='_blank' href='uploadvideo.php?class=$class&stream=$stream&subject=$subject&section=$section&chapter=$chapter'>Add New Video</a>";
+        echo "<a class='btn btn-primary btn-sm rounded-pill px-3' target='_blank' href='uploadnotes.php?class=$class&stream=$stream&subject=$subject&section=$section&chapter=$chapter'>Add New Notes</a>";
+        echo "<a class='btn btn-primary btn-sm rounded-pill px-3' target='_blank' href='uploadvideo.php?class=$class&stream=$stream&subject=$subject&section=$section&chapter=$chapter'>Add New Video</a>";
         echo "</div></div>";
     }
 ?>
